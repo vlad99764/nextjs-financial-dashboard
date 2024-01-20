50306059ac87e817245c5573199da56551ab3f88
vlad99764 <vladyslavpiankov@gmail.com>
2024-01-20 13:53:06 +0600

    use debounce on search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,20 +2,33 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-
+import { useDebouncedCallback } from 'use-debounce';
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  function handleSearch(term: string) {
+
+  // function handleSearch(term: string) {
+  //   const params = new URLSearchParams(searchParams);
+  //   if(term){
+  //     params.set('query',term);
+  //   }else{
+  //     params.delete('query');
+  //   }
+  //   replace(`${pathname}?${params.toString()}`);
+  // }
+
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching ... ${term}`);
     const params = new URLSearchParams(searchParams);
-    if(term){
+    if (term) {
       params.set('query',term);
-    }else{
+    } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  },3000)
+  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
